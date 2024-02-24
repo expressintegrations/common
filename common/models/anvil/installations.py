e449639cd337bece1261d9ed5b10772677074b8a
Jasper Madrone <jasper@expressintegrations.com>
2024-02-23 21:17:46 -0500

    initial commit

--- common/models/anvil/installations.py
+++ common/models/anvil/installations.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -5,5 +7,5 @@ class InstallationRequest(BaseModel):
     connection_id: str
     user_id: str
     integration_name: str
-    installation_id: str
+    installation_id: Optional[str] = None
     inputs: dict
