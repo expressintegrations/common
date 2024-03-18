9b17c9a8bf221516b0f0bba021bd08a3827aafba
expressintegrations <jasper@expressintegrations.com>
2024-03-18 18:25:37 -0400

    migration

--- common/models/monday/integration_runs.py
+++ common/models/monday/integration_runs.py
@@ -1,15 +1,25 @@
+from typing import Any
 from typing import Optional
 
 from pydantic import BaseModel
 from pydantic.alias_generators import to_camel
 
 
+class Reference(BaseModel):
+    value: Any
+
+
 class InboundFieldValues(BaseModel):
     board_id: Optional[int] = None
-    table: Optional[str] = None
+    table: Optional[Reference] = None
+    table_column: Optional[Reference] = None
+    boolean_column: Optional[Reference] = None
+    boolean_value: Optional[Reference] = None
     schema: Optional[str] = None
     table_name: Optional[str] = None
     item_id: Optional[str] = None
+    error_column_id: Optional[str] = None
+    row: Optional[dict] = None
 
     class Config:
         populate_by_name = True
@@ -20,6 +30,8 @@ class IntegrationRun(BaseModel):
     recipe_id: int
     integration_id: int
     inbound_field_values: InboundFieldValues
+    account_id: Optional[int] = None
+    user_id: Optional[int] = None
 
     class Config:
         populate_by_name = True
