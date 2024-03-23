5170acf3b95cd10f006b6a9227c30c73fe4f6ed2
expressintegrations <jasper@expressintegrations.com>
2024-03-22 20:10:28 -0400

    migration

--- common/services/firestore.py
+++ common/services/firestore.py
@@ -472,7 +472,7 @@ class FirestoreService(BaseService):
                 op_string="==",
                 value=False
             )
-        ).limit(1000)
+        )
 
 
 class ConnectionNotFoundException(Exception):
