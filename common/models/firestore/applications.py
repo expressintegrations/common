c87b0e2cd929d6ea4365ae69b3b8644c0361a647
Jasper Madrone <jasper@expressintegrations.com>
2024-05-12 14:04:41 -0400

    monday

--- common/models/firestore/applications.py
+++ common/models/firestore/applications.py
@@ -18,7 +18,7 @@ class FieldInput(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
     label: Optional[str] = None
-    dependent_field_values: Optional[Dict[str, List[DependentField]]]
+    dependent_field_values: Optional[Dict[str, List[DependentField]]] = None
     dependent_on_field: Optional[str] = None
     dependent_on_field_value: Optional[Any] = None
     type: Optional[str] = None
