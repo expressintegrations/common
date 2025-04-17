2e2953915cffb71f4c1d351686dbdcd608347921
Jasper Madrone <jasper@expressintegrations.com>
2025-04-16 21:16:29 -0400

    style(users): format code for consistency and readability

--- common/models/firestore/users.py
+++ common/models/firestore/users.py
@@ -4,7 +4,7 @@ from firedantic import Model
 
 
 class User(Model):
-    __collection__ = 'users'
+    __collection__ = "users"
     email: Optional[str] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
@@ -12,14 +12,22 @@ class User(Model):
     hs_contact_id: Optional[str] = None
     anvil_user_id: Optional[str] = None
     monday_user_id: Optional[int] = None
+    monday_account_id: Optional[int] = None
 
-    def save(self, by_alias: bool = True, exclude_unset: bool = True, exclude_none: bool = False) -> None:
+    def save(
+        self,
+        by_alias: bool = True,
+        exclude_unset: bool = True,
+        exclude_none: bool = False,
+    ) -> None:
         """
         Saves this model in the database.
 
         :raise DocumentIDError: If the document ID is not valid.
         """
-        data = self.model_dump(by_alias=by_alias, exclude_unset=exclude_unset, exclude_none=exclude_none)
+        data = self.model_dump(
+            by_alias=by_alias, exclude_unset=exclude_unset, exclude_none=exclude_none
+        )
         if self.__document_id__ in data:
             del data[self.__document_id__]
 
