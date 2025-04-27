318d334d62a5a28e0ba6ebe7959bea450657642c
Jasper Madrone <jasper@expressintegrations.com>
2025-04-26 20:23:22 -0400

    feat(MondayAccountRecipes): refactor IntegrationAlert to use Model and simplify structure

--- common/models/monday/monday_account_recipes.py
+++ common/models/monday/monday_account_recipes.py
@@ -3,6 +3,17 @@ from typing import Optional
 from enum import StrEnum
 
 from firedantic import Model, SubModel, SubCollection
+from pydantic import BaseModel
+
+
+class MondayAccountConnection(BaseModel):
+    installation_id: str
+    connection_id: str
+    app_name: str
+    authorized_by_id: str
+    user_account_identifier: str
+    connected: bool
+    icon: str
 
 
 class MondayAccountRecipe(Model):
@@ -62,33 +73,18 @@ class AlertType(StrEnum):
     INFO = "info"
 
 
-class IntegrationAlert(SubModel):
+class IntegrationAlert(Model):
+    __collection__ = "monday_integration_alerts"
     type: AlertType
     text: str
     link: Optional[str] = None
     link_text: Optional[str] = None
     dismissed: Optional[bool] = False
 
-    class Collection(SubCollection):
-        __collection_tpl__ = "monday_account_recipes/{id}/integration_alerts"
 
-    def save(
-        self,
-        by_alias: bool = True,
-        exclude_unset: bool = False,
-        exclude_none: bool = False,
-    ) -> None:
-        """
-        Saves this model in the database.
-
-        :raise DocumentIDError: If the document ID is not valid.
-        """
-        data = self.model_dump(
-            by_alias=by_alias, exclude_unset=exclude_unset, exclude_none=exclude_none
-        )
-        if self.__document_id__ in data:
-            del data[self.__document_id__]
-
-        doc_ref = self._get_doc_ref()
-        doc_ref.set(data)
-        setattr(self, self.__document_id__, doc_ref.id)
+class MondayAccountSettings(BaseModel):
+    account_id: str
+    user_id: str
+    connections: list[MondayAccountConnection]
+    recipes: list[MondayAccountRecipe]
+    alerts: list[IntegrationAlert]
